ea2aaabb0e906f028037b3b15a0a7c56beac257b
MohamedAmr23 <amrm96754@gmail.com>
2025-01-18 04:37:55 +0200

    Jaden Casing Strings

--- Shortest-Word.js
+++ Shortest-Word.js
@@ -1,5 +1,9 @@
 function findShort(s){
-    return Math.min(...s.split(' ').map((word)=>word.length))
+    // return Math.min(...s.split(' ').map((word)=>word.length))
+
+    // another solution
+
+    return s.split(' ').sort((a,b)=>b.length - a.length).pop().length
 }
 
 console.log(findShort('mohamed amr ahmed'))
